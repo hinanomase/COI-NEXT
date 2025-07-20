5af86f02c930d1a8f92f06f32119cf70a716658c
papirosuke <hiro20020531@gmail.com>
2025-07-21 05:09:13 +0900

    add chat completion

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -2,8 +2,8 @@
  * 定数の管理
  */
 
-// export const BACKEND_URL = "https://coi-next.onrender.com";
-export const BACKEND_URL = "http://127.0.0.1:8000";
+export const BACKEND_URL = "https://coi-next.onrender.com";
+// export const BACKEND_URL = "http://127.0.0.1:8000";
 export const PROXY_ENDPOINT = `${BACKEND_URL}/api/realtime-proxy`;
 export const TRANSCRIPT_PROXY_ENDPOINT = `${BACKEND_URL}/api/transcription-proxy`;
 export const TTS_ENDPOINT = `${BACKEND_URL}/api/tts`;
